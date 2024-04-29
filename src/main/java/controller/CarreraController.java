389bc2ed310b32910779dfee0caf6711f53d150c
Leonardo Roa <l.roa01@ufromail.cl>
2024-04-28 22:42:41 -0400

    avanzando en el codigo y agregando ambos Controller

--- src/main/java/controller/CarreraController.java
+++ src/main/java/controller/CarreraController.java
@@ -1,6 +1,29 @@
 package controller;
 
+import modelo.Carrera;
+import modelo.data.dao.CarreraDAO;
+import modelo.data.DBConnector;
+import modelo.data.DBGenerator;
+import org.jooq.DSLContext;
 public class CarreraController {
 
+    public static boolean agregarCarrera(String nombreCarrera, String codigoCarrera, int nSemestres) throws ClassNotFoundException{
+        DSLContext query = DBGenerator.conectarBD("Intranet");
+        if(!CarreraDAO.validarExistenciaCarrera(query,"codigo",codigoCarrera)){
+            Carrera carrera = new Carrera(nombreCarrera,codigoCarrera,nSemestres);
+            CarreraDAO.agregarCarrera(query,carrera);
+            DBConnector.closeConnection();
+            return true;
+        }
+        else{
+            return false;
+        }
 
+        public static Object[] getCodigoCarreras() throws ClassNotFoundException {
+            DSLContext query = DBGenerator.conectarBD("Intranet");
+            Object[] carreras = CarreraDAO.getCodigoCarreras(query);
+            DBConnector.closeConnection();
+            return carreras;
+        }
+    }
 }
--- src/main/java/controller/EstudianteController.java
+++ src/main/java/controller/EstudianteController.java
@@ -1,4 +1,42 @@
 package controller;
 
+import modelo.Carrera;
+import modelo.Estudiante;
+import modelo.data.dao.CarreraDAO;
+import modelo.data.DBConnector;
+import modelo.data.DBGenerator;
+import modelo.data.dao.EstudianteDAO;
+import org.jooq.DSLContext;
+
 public class EstudianteController {
+    public static boolean registrarEstudiante(String nombre, String rut, String matricula, String codigoCarrera) throws ClassNotFoundException{
+        DSLContext query = DBGenerator.conectarBD("Intranet");
+        if(!EstudianteDAO.validarExistenciaEstudiante(query,"rut",rut)){
+            Carrera carrera =CarreraDAO.buscarCarrera(query,codigoCarrera);
+            Estudiante estudiante= new Estudiante(rut,nombre,matricula,carrera);
+            EstudianteDAO.agregarEstudiante(query,estudiante);
+            DBConnector.closeConnection();
+            return true;
+        }
+        else{
+            DBConnector.closeConnection();
+            return false;
+        }
+    }
+
+    public static String[][] mostrarEstudiantesPorCarrera(String codigo) throws ClassNotFoundException {
+        DSLContext query = DBGenerator.conectarBD("Intranet");
+        String[][] datosEstudiantes= EstudianteDAO.obtenerEstudiantesCursoCodigo(query,codigo);
+        DBConnector.closeConnection();
+        return datosEstudiantes;
+    }
+
+    public static String[][] mostrarEstudiantesPorNombre(String codigo, String nombre) throws ClassNotFoundException {
+        DSLContext query = DBGenerator.conectarBD("Intranet");
+        String[][] datosEstudiantes = EstudianteDAO.obtenerEstudiantesCursoNombre(query,nombre,codigo);
+        DBConnector.closeConnection();
+        return datosEstudiantes;
+    }
+
+
 }
