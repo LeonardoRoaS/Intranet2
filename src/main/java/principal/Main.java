0611042c65217b7ab61c5e942434fe8594b4bd88
Leonardo Roa <l.roa01@ufromail.cl>
2024-04-28 22:33:39 -0400

    avanzando en el codigo y subiendo el pom.xml con xampp enlazado

--- src/main/java/principal/Main.java
+++ src/main/java/principal/Main.java
@@ -1,9 +1,12 @@
 package principal;
 
 import gui.VentanaBienvenida;
+import modelo.data.DBConnector;
+import modelo.data.DBGenerator;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException{
+        DBGenerator.iniciarBD("Intranet");
         VentanaBienvenida test = new VentanaBienvenida();
     }
 }
