25d9ce21886f507e6ad484030e96bf1bb85e6e3e
Leonardo Roa <l.roa01@ufromail.cl>
2024-04-28 20:43:29 -0400

    Subiendo primeras clases

--- src/main/java/modelo/Carrera.java
+++ src/main/java/modelo/Carrera.java
@@ -1,4 +1,36 @@
 package modelo;
 
 public class Carrera {
+    private String nombreCarrera, codigoCarrera;
+    private int nSemestres;
+
+    public Carrera(String nombreCarrera, String codigoCarrera, int nSemestres) {
+        this.nombreCarrera = nombreCarrera;
+        this.codigoCarrera = codigoCarrera;
+        this.nSemestres = nSemestres;
+    }
+
+    public String getNombreCarrera() {
+        return nombreCarrera;
+    }
+
+    public void setNombreCarrera(String nombreCarrera) {
+        this.nombreCarrera = nombreCarrera;
+    }
+
+    public String getCodigoCarrera() {
+        return codigoCarrera;
+    }
+
+    public void setCodigoCarrera(String codigoCarrera) {
+        this.codigoCarrera = codigoCarrera;
+    }
+
+    public int getnSemestres() {
+        return nSemestres;
+    }
+
+    public void setnSemestres(int nSemestres) {
+        this.nSemestres = nSemestres;
+    }
 }
--- src/main/java/modelo/Estudiante.java
+++ src/main/java/modelo/Estudiante.java
@@ -1,4 +1,29 @@
 package modelo;
 
 public class Estudiante {
+    private String nombre, rut, matricula;
+    private Carrera carrera;
+
+    public Estudiante (String nombre, String rut, String matricula, Carrera carrera){
+        this.nombre = nombre;
+        this.rut = rut;
+        this.matricula = matricula;
+        this.carrera = carrera;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public Carrera getCarrera() {
+        return carrera;
+    }
 }
